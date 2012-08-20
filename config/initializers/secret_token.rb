281e8afccf5f663aaedf7112b8e6afd9f06cca4b
TMapp <streetmapp@gmail.com>
2012-08-20 23:33:45 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5b3112822836ffa3062b6bf3bf9385ba6f439e42e4f6a8cbcbddc8899f958b592716a1a1c4cf428c8e2ab0801af675fcb0940271e117d3a1575924dd20131f17'
